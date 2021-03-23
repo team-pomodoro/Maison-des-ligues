db6feb92ae274f0c04e90ea236dcaf8955179669
Neguro <nassim.ahmedali@gmail.com>
2021-03-23 11:54:02 +0100

    ajout entity licencie

---
+++ src/Repository/LicencieRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Licencie;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Licencie|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Licencie|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Licencie[]    findAll()
+ * @method Licencie[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LicencieRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Licencie::class);
+    }
+
+    // /**
+    //  * @return Licencie[] Returns an array of Licencie objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Licencie
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
