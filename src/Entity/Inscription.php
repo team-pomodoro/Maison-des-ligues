31ca0d21d8f12eb9f4a67335f8813e5c4b3231b9
Neguro <nassim.ahmedali@gmail.com>
2021-03-23 12:29:06 +0100

    ajout entité inscription

---
+++ src/Entity/Inscription.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InscriptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InscriptionRepository::class)
+ */
+class Inscription
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateInscription;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $dateInscription): self
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+}
